d146160d018799ffe5f712e3c6e4dad45caea2ed
Arindam Sankar Das <arindamsankar@outlook.com>
2023-04-11 14:43:08 +0530

    added script

--- index.js
+++
@@ -1,7 +0,0 @@
-const nav = document.getElementsByClassName("navbar");
-const toggler = document.getElementById("toggler");
-
-toggler.addEventListener("click", () => {
-  let [classNames] = nav;
-  classNames.classList.toggle("-left-full");
-});
